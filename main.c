cab573a15fb7cdcc97c0c693362ca5f82eb837bb
VitorAbella <vitor.abella@gmail.com>
2023-07-17 10:48:35 -0300

    - portal added\n- (i,j) used directly for coordinates\n- code adapted to read map as string

--- main.c
+++ main.c
@@ -4,15 +4,20 @@
 
 int main(void)
 {
-
+	int i,j,missionComplete=0;
 	int MAP_SIZE_PX_L=MAP_SIZE_L*20;
 	int MAP_SIZE_PX_C=MAP_SIZE_C*20;
     int x=MAP_SIZE_PX_C/2,y=MAP_SIZE_PX_L/2;
+    
+    // set initial position
+    i=MAP_SIZE_L/2;
+    j=MAP_SIZE_C/2;
+    
     readMap();
 
     //--------------------------------------------------------------------------------------
     //Inicializa¸c~oes
-    InitWindow(MAP_SIZE_PX_C, MAP_SIZE_PX_L, "Quadrado");//Inicializa janela, com certo tamanho e t´ıtulo
+    InitWindow(MAP_SIZE_PX_C, MAP_SIZE_PX_L, "The Binding of Isaac remastered");//Inicializa janela, com certo tamanho e t´ıtulo
     SetTargetFPS(60);// Ajusta a execu¸c~ao do jogo para 60 frames por segundo
     //--------------------------------------------------------------------------------------
     //La¸co principal do jogo
@@ -22,39 +27,47 @@ int main(void)
     //----------------------------------------------------------------------------------
         if (IsKeyPressed(KEY_RIGHT))
         {
-            x+=20;
-            if(matrixMap[y/20][x/20]==1)
+            j+=1;
+            if(map[i][j]=='#')
             {
-                x-=20;
+                j-=1;
             }
         }
         if (IsKeyPressed(KEY_LEFT))
         {
-            x-=20;
-            if(matrixMap[y/20][x/20]==1)
+            j-=1;
+            if(map[i][j]=='#')
             {
-                x+=20;
+                j+=1;
             }
         }
         if (IsKeyPressed(KEY_UP))
         {
-            y-=20;
-            if(matrixMap[y/20][x/20]==1)
+            i-=1;
+            if(map[i][j]=='#')
             {
-                y+=20;
+                i+=1;
             }
         }
         if (IsKeyPressed(KEY_DOWN))
         {
-            y+=20;
-            if(matrixMap[y/20][x/20]==1)
+            i+=1;
+            if(map[i][j]=='#')
             {
-                y-=20;
+                i-=1;
             }
         }
+        if(map[i][j]=='P')
+        {
+			missionComplete=1;
+			CloseWindow();
+		}
         //----------------------------------------------------------------------------------
         // Atualiza a representa¸c~ao visual a partir do estado do jogo
         //----------------------------------------------------------------------------------
+        x=j*20;
+        y=i*20;
+        
         BeginDrawing();//Inicia o ambiente de desenho na tela
         ClearBackground(RAYWHITE);//Limpa a tela e define cor de fundo
         DrawRectangle(x,y,20,20,GREEN);
@@ -63,10 +76,16 @@ int main(void)
         {
             for(int j=0; j<MAP_SIZE_C; j++)
             {
-                if(matrixMap[i][j]==1)
+				//walls
+                if(map[i][j]=='#')
                 {
                     DrawRectangle(j*20,i*20,20,20,RED);
                 }
+                //portal
+                else if(map[i][j]=='P')
+                {
+					DrawRectangle(j*20,i*20,20,20,YELLOW);
+				}
             }
         }
         EndDrawing();//Finaliza o ambiente de desenho na tela
