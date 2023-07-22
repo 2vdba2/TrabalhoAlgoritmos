ab46f3b583244a4c15f6f09cf3ed3d0f77da3b34
VitorAbella <vitor.abella@gmail.com>
2023-07-22 19:29:09 -0300

    update install

--- Others/InstallRaylibUbuntu/install.sh
+++ Others/InstallRaylibUbuntu/install.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt install build-essential git
 
 sudo apt install libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev
